3214ad7f21ee20f0446c932ca46ba537a6d243c4
Miguel Balleza <maballeza@uh.edu>
2021-05-13 23:02:31 -0400

    Modified connection configuration to correctly retrieve SQL timestamps.

--- deliveries.js
+++ deliveries.js
@@ -1,33 +1,8 @@
-var sql = require('mssql/msnodesqlv8');
+const sql = require("msnodesqlv8");
 
-const config = {
-    database: 'SampleDB',
-    server: 'localhost',
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 3000
-    },
-    options: {
-        trustedConnection: true
-    }
-};
+const connectionString = "server=localhost;Database=AdventureWorks;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT TOP 10 oh.SalesOrderID, oh.DueDate, oh.ShipToAddressID, oh.Freight, oh.AccountNumber, oh.Status, oh.Comment FROM AdventureWorks.Sales.SalesOrderHeader oh ORDER BY oh.DueDate DESC";
 
-sql.connect(config).then(() => {
-    return sql.query`
-        SELECT TOP 10 
-            oh.SalesOrderID, 
-            oh.DueDate, 
-            oh.ShipToAddressID, 
-            oh.Freight, 
-            oh.AccountNumber, 
-            oh.Status,
-            oh.Comment
-        FROM AdventureWorks.Sales.SalesOrderHeader oh 
-        ORDER BY oh.DueDate 
-        DESC;`
-}).then(result => {
-    console.dir(result)
-}).catch(err => {
-    console.dir(err)
+sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
 });
